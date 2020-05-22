8a32dbcedbae4adff601f7fb2dfdaaa61f326f1b
BM Abir <bm.abir@chowagiken.co.jp>
2020-05-22 06:24:03 +0600

    slide in animation for listed exp,edu and project

--- people/abir/index.js
+++ people/abir/index.js
@@ -1,3 +1,4 @@
+
 window.onload = function () {
 	var dateMod= document.lastModified;
 	//alert(dateMod);
@@ -5,3 +6,32 @@ window.onload = function () {
 	var elemShowdate= document.getElementById('showModDate');
 	elemShowdate.innerHTML=dateMod;
 }
+// window.onscroll = function() {animate()};
+
+// function animate() {
+// 	console.log("scroll")
+// 	var element =$("#scroll-scene");
+// 	if(element.scrollTop() > 0) {
+// 	element.addClass("animate");
+// 	console.log("scroll added class")
+	
+// 	}
+	
+	
+// };
+$(function () {
+    var element = $(".scroll-scene");
+    $(window).scroll(function () {
+      if($(window).scrollTop() > 0) {
+		element.addClass("animate");
+      }
+      
+	});
+	// var element2 = $(".scroll-scene2");
+    // element2.scroll(function () {
+    //   if($(window).scrollTop() > 0) {
+	// 	element2.addClass("animate");
+    //   }
+      
+    // });
+});
